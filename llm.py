2091171681bc62309f4042478136cbde8de8779e
Dominic Gallego <65346693+DomGallego@users.noreply.github.com>
2024-05-15 22:54:47 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -103,6 +103,7 @@ def generate_llm_response(chat_history):
         "question. If you don't know the answer, just say that you "
         "don't know."
         "{context}"
+    )
     # qa_system_prompt = (
     #     """You are an assistant for question-answering tasks. Use 
     #     the following pieces of retrieved context/document to answer the 
